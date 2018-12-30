dfc8c701b27afb2894b61d46230d584cb67c0933
pawelczuczwara <pawel.czuczwara@gmail.com>
2018-12-30 20:50:56 +0100

    V1.0 working notes with date

---
+++ README.md
@@ -0,0 +1,5 @@
+# Udacity Retain
+
+This is an example how to use MV* frameworks.
+
+Clone the repo and open up `index.html` in your browser of choice.
---
+++ js/retain.js
@@ -0,0 +1,63 @@
+$(function(){
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        }
+    };
+
+
+    var octopus = {
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr,
+                date: Date.now()
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        }
+    };
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+            newNoteForm.submit(function(e){
+                octopus.addNewNote(newNoteContent.val());
+                newNoteContent.val('');
+                e.preventDefault();
+            });
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            var options = { year: 'numeric', month: 'short', day: '2-digit'};
+            octopus.getNotes().forEach(function(note){
+                var _resultDate = new Intl.DateTimeFormat('en-GB', options).format(note.date);
+                htmlStr += `<li class='note'>${note.content}<span class='note-date'>${_resultDate}</span></li>`;
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
